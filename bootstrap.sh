9718252ffe928ec3294c725b431d47e10c478176
Phil Lee <asmega@ph-lee.com>
2018-07-14 09:33:54 +0100

    add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+ln -s /Volumes/git/dotfiles/.git /Users/asmega/.git 
+ln -s /Volumes/git/dotfiles/.gitconfig /Users/asmega/.gitconfig
+ln -s /Volumes/git/dotfiles/.gitignore /Users/asmega/.gitignore
+ln -s /Volumes/git/dotfiles/.iterm /Users/asmega/.iterm
+ln -s /Volumes/git/dotfiles/.vimrc /Users/asmega/.vimrc
+ln -s /Volumes/git/dotfiles/.zshrc /Users/asmega/.zshrc
