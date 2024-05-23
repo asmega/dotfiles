0dfa03a261006540dc06b51c8dd937c9e6b9c110
Phil Lee <asmega@users.noreply.github.com>
2024-05-23 22:48:58 +0100

    wezterm tab styling

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,6 +6,17 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
+config.use_fancy_tab_bar = false
+
+config.colors = {
+  tab_bar = {
+    active_tab = {
+      bg_color = '#406d24',
+      fg_color = '#c0c0c0',
+    }
+  }
+}
+
 local act = wezterm.action
 
 config.keys = {
